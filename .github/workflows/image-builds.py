3e220efe652ad06da8b3899de9857447652048cf
Michael Still <mikal@stillhq.com>
2022-12-20 20:48:21 +1100

    Simply image builds to not rebuild their base images.

--- .github/workflows/image-builds.py
+++ .github/workflows/image-builds.py
@@ -5,45 +5,33 @@ import jinja2
 JOBS = [
     {
         'name': 'debian-10',
-        'baseimage': 'sf://label/system/sfci-image-publisher-debian-10',
-        'baseuser': 'debian',
-        'label': 'sfci-image-publisher-debian-10',
-        'image': 'debian:10'
+        'baseimage': 'debian:11',
+        'baseuser': 'debian'
     },
     {
         'name': 'debian-11',
-        'baseimage': 'sf://label/system/sfci-image-publisher-debian-11',
-        'baseuser': 'debian',
-        'label': 'sfci-image-publisher-debian-11',
-        'image': 'debian:11'
+        'baseimage': 'debian:11',
+        'baseuser': 'debian'
     },
     {
         'name': 'ubuntu-1804',
-        'baseimage': 'sf://label/system/sfci-image-publisher-ubuntu-1804',
-        'baseuser': 'debian',
-        'label': 'sfci-image-publisher-ubuntu-1804',
-        'image': 'ubuntu:18.04'
+        'baseimage': 'debian:11',
+        'baseuser': 'debian'
     },
     {
         'name': 'ubuntu-2004',
-        'baseimage': 'sf://label/system/sfci-image-publisher-ubuntu-2004',
-        'baseuser': 'debian',
-        'label': 'sfci-image-publisher-ubuntu-2004',
-        'image': 'ubuntu:20.04'
+        'baseimage': 'debian:11',
+        'baseuser': 'debian'
     },
     {
         'name': 'centos-7',
-        'baseimage': 'sf://label/system/sfci-image-publisher-centos-7',
-        'baseuser': 'debian',
-        'label': 'sfci-image-publisher-centos-7',
-        'image': 'centos:7'
+        'baseimage': 'debian:11',
+        'baseuser': 'debian'
     },
     {
         'name': 'centos-8',
-        'baseimage': 'sf://label/system/sfci-image-publisher-centos-8-stream',
-        'baseuser': 'debian',
-        'label': 'sfci-image-publisher-centos-8-stream',
-        'image': 'centos:8-stream'
+        'baseimage': 'debian:11',
+        'baseuser': 'debian'
     }
 ]
 
